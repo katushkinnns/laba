9dafb6fe744c37610d140e1568ea5b2438d13900
katushkinnns <komarenkoekaterina5@gmail.com>
2024-11-10 16:09:33 +0300

    bez oshibok

--- laba5.py
+++ laba5.py
@@ -1,5 +1,10 @@
 print('Задание №1')
-number=int(input('Ведите целое число: '))
+while True:
+    try:
+        number= int(input("Введите целое число: ", ))
+        break
+    except ValueError:
+        print(" Попробуй ещё раз, спасибо!")
 number=abs(number)
 num=str(number)
 summ=0
@@ -10,24 +15,57 @@ for i in num:
 print('Сумма цифр: ', summ)
 print('Произведение цифр: ', mult)
 
-print('Задание №2')
+('Задание №2')
 import math
-X=float(input('Введите вещественное число:'))
-N=int(input('Введите целое число, N>0 :'))
+while True:
+    while True:
+       try: N= float(input('Введите вещественное число X: '))
+       except ValueError:
+           print('Пожалуйста, попробуйте еще раз,спасибо!')
+       else: break
+    if N <= 0:
+        print('Введено неположительное число')
+    else: break
+while True:
+
+    while True:
+       try: N= int(input('Введите целое число, N>0: '))
+       except ValueError:
+           print('Пожалуйста, попробуйте еще раз,спасибо!')
+       else: break
+    if N <= 0:
+        print('Введено неположительное число')
+    else: break
 summ=1
 for i in range(1,N+1):
     summ+=X**i/(math.factorial(i))
 print('Полученное число является приближенным значением функции exp в точке X:', math.exp(X))
-
 print('Задание №3')
-def gcd(a, b):
+def gcd(a,b):
     while b:
         a, b = b, a % b
     return a
-A = int(input("Введите первое положительное число A: "))
-B = int(input("Введите второе положительное число B: "))
-if A > 0 and B > 0:
-    result = gcd(A, B)
-    print(f"Наибольший общий делитель {A} и {B} равен {result}.")
-else:
-    print("Пожалуйста, введите положительные целые числа.")
+while True:
+    while True:
+       try: A= int(input('Введите первое положительное число A: '))
+       except ValueError:
+           print('Пожалуйста, попробуйте еще раз,спасибо!')
+       else: break
+    if A <= 0:
+        print('Введено неположительное число')
+    else: break
+while True:
+    while True:
+        try:
+            B= int(input('Введите второе положительное число B: '))
+        except ValueError:
+            print('Пожалуйста, попробуйте еще раз,спасибо!')
+        else:
+            break
+    if B <= 0:
+        print('Введено неположительное число')
+    else:
+        break
+
+result = gcd(A, B)
+print(f"Наибольший общий делитель {A} и {B} равен {result}.")
